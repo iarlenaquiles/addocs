36849536b5096742770cd57937e975aed8dda04e
Iarlen Aquiles <iarlensilva775@gmail.com>
2021-03-19 14:15:44 -0300

    fix: routes removed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const routes = require('./routes');
-
 const app = express();
 
 app.use(helmet());
-
-app.use(routes);
 
 module.exports = app;
--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-router.get('/healthcheck', (req, res) => res.status(200).send({ status: 'healthy' }));
-
-module.exports = router;
